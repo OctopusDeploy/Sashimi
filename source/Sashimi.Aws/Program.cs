85920967dba5e4e0f8e4a67824369a35c5518c06
benpearce1 <ben.pearce@octopus.com>
2020-05-12 11:58:00 +1000

    initial check-in for AWS

---
+++ source/Sashimi.Aws/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sashimi.Aws
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
