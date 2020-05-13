0158c3de87fdb953bfd072b222c618a7882bb9fb
Andy Hsu <andy.hsu@octopus.com>
2020-05-13 16:48:04 +1000

    Refactor UploadAwsS3Command

--- source/Calamari.Aws/Program.cs
+++ source/Calamari.Aws/Program.cs
@@ -15,7 +15,45 @@ namespace Calamari.Aws
         public Program(ILog log) : base(log)
         {
         }
-        
+
+        protected override void ConfigureContainer(ContainerBuilder builder, CommonOptions options)
+        {
+            builder.Register(
+                    c => AwsEnvironmentGeneration.Create(log, c.Resolve<IVariables>()).GetAwaiter().GetResult())
+                .AsSelf()
+                .SingleInstance();
+
+            builder.Register(c =>
+            {
+                var environment = c.Resolve<AwsEnvironmentGeneration>();
+
+                return new AmazonS3Client(environment.AwsCredentials,
+                    environment.AsClientConfig<AmazonS3Config>());
+            }).As<IAmazonS3>();
+
+            builder.Register(c =>
+            {
+                var environment = c.Resolve<AwsEnvironmentGeneration>();
+
+                return new AmazonIdentityManagementServiceClient(environment.AwsCredentials,
+                    environment.AsClientConfig<AmazonIdentityManagementServiceConfig>());
+            }).As<IAmazonIdentityManagementService>();
+
+            builder.Register(c =>
+            {
+                var environment = c.Resolve<AwsEnvironmentGeneration>();
+
+                return new AmazonSecurityTokenServiceClient(environment.AwsCredentials,
+                    environment.AsClientConfig<AmazonSecurityTokenServiceConfig>());
+            }).As<IAmazonSecurityTokenService>();
+
+            builder.RegisterType<VariableS3TargetOptionsProvider>()
+                .As<IProvideS3TargetOptions>()
+                .SingleInstance();
+
+            base.ConfigureContainer(builder, options);
+        }
+
         public static int Main(string[] args)
         {
             return new Program(ConsoleLog.Instance).Run(args);
