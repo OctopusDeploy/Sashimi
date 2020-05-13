7af4b20e975b62a8096c50ed71e4665d2e569247
Andy Hsu <andy.hsu@octopus.com>
2020-05-13 16:48:04 +1000

    Refactor UploadAwsS3Command

--- source/Calamari.Aws/Program.cs
+++ source/Calamari.Aws/Program.cs
@@ -1,4 +1,12 @@
-﻿namespace Calamari.Aws
+﻿using Amazon.IdentityManagement;
+using Amazon.S3;
+using Amazon.SecurityToken;
+using Autofac;
+using Calamari.Aws.Integration.S3;
+using Calamari.Aws.Util;
+using Calamari.CloudAccounts;
+
+namespace Calamari.Aws
 {
     public class Program : CalamariFlavourProgram
     {
@@ -6,7 +14,45 @@
         Program() : base(ConsoleLog.Instance)
         {
         }
-        
+
+        protected override void ConfigureContainer(ContainerBuilder builder, CommonOptions options)
+        {
+            builder.Register(
+                    c => AwsEnvironmentGeneration.Create(log, c.Resolve<IVariables>()).GetAwaiter().GetResult())
+                .AsSelf()
+                .SingleInstance();
+
+            builder.Register(c =>
+            {
+                var environment = c.Resolve<AwsEnvironmentGeneration>();
+
+                return new AmazonS3Client(environment.AwsCredentials,
+                    environment.AsClientConfig<AmazonS3Config>());
+            }).As<IAmazonS3>();
+
+            builder.Register(c =>
+            {
+                var environment = c.Resolve<AwsEnvironmentGeneration>();
+
+                return new AmazonIdentityManagementServiceClient(environment.AwsCredentials,
+                    environment.AsClientConfig<AmazonIdentityManagementServiceConfig>());
+            }).As<IAmazonIdentityManagementService>();
+
+            builder.Register(c =>
+            {
+                var environment = c.Resolve<AwsEnvironmentGeneration>();
+
+                return new AmazonSecurityTokenServiceClient(environment.AwsCredentials,
+                    environment.AsClientConfig<AmazonSecurityTokenServiceConfig>());
+            }).As<IAmazonSecurityTokenService>();
+
+            builder.RegisterType<VariableS3TargetOptionsProvider>()
+                .As<IProvideS3TargetOptions>()
+                .SingleInstance();
+
+            base.ConfigureContainer(builder, options);
+        }
+
         public static int Main(string[] args)
         {
             return new Program().Run(args);
