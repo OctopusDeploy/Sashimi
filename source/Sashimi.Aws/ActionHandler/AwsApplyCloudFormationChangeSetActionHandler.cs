65ec149b1e952425371d2d3e7bc008ebbfafe334
benpearce1 <ben.pearce@octopus.com>
2020-05-14 10:44:41 +1000

    Initial work on AWS Sashimi

---
+++ source/Sashimi.Aws/ActionHandler/AwsApplyCloudFormationChangeSetActionHandler.cs
@@ -0,0 +1,35 @@
+﻿using Sashimi.Server.Contracts;
+using Sashimi.Server.Contracts.ActionHandlers;
+using Sashimi.Server.Contracts.Calamari;
+
+namespace Sashimi.Aws.ActionHandler
+{
+    /// <summary>
+    /// The action handler that prepares a Calamari script execution with
+    /// the path set to include the AWS CLI and having AWS credentials
+    /// set in the common environment variable paths. It then goes on to
+    /// deploy a cloud formation template.
+    /// </summary>
+    public class AwsApplyCloudFormationChangeSetActionHandler : IActionHandler
+    {
+        public string Id => AwsActionTypes.ApplyCloudFormationChangeset;
+        public string Name => "Apply an AWS CloudFormation Change Set";
+        public string Description => "Applies an existing AWS CloudFormation Change Set";
+        public string Keywords => null;
+        public bool ShowInStepTemplatePickerUI => true;
+        public bool WhenInAChildStepRunInTheContextOfTheTargetMachine => false;
+        public bool CanRunOnDeploymentTarget => false;
+        public ActionHandlerCategory[] Categories => new[] { ActionHandlerCategory.BuiltInStep, AwsConstants.AwsActionHandlerCategory };
+
+        public IActionHandlerResult Execute(IActionHandlerContext context)
+        {
+            var builder = context.CalamariCommand(CalamariFlavour.CalamariAws, "apply-aws-cloudformation-changeset");
+
+            builder.WithArgument("waitForCompletion", context.Variables.GetFlag(AwsSpecialVariables.Action.Aws.WaitForCompletion, true).ToString());
+            builder.WithArgument("stackName", context.Variables.Get(AwsSpecialVariables.Action.Aws.CloudFormation.StackName, ""));
+            builder.WithArgument("extensions", CalamariExtensions.Aws);
+
+            return builder.Execute();
+        }
+    }
+}
---
+++ source/Sashimi.Aws/ActionHandler/AwsUploadS3ActionHandler.cs
@@ -0,0 +1,26 @@
+﻿using Sashimi.Server.Contracts;
+using Sashimi.Server.Contracts.ActionHandlers;
+using Sashimi.Server.Contracts.Calamari;
+
+namespace Sashimi.Aws.ActionHandler
+{
+    public class AwsUploadS3ActionHandler : IActionHandler
+    {
+        public string Id => AwsActionTypes.UploadS3;
+        public string Name => "Upload a package to an AWS S3 bucket";
+        public string Description => "Upload a package or package contents to an AWS S3 bucket.";
+        public string Keywords => string.Empty;
+        public bool ShowInStepTemplatePickerUI => true;
+        public bool WhenInAChildStepRunInTheContextOfTheTargetMachine => false;
+        public bool CanRunOnDeploymentTarget => true;
+        public ActionHandlerCategory[] Categories => new[] { ActionHandlerCategory.BuiltInStep, AwsConstants.AwsActionHandlerCategory, ActionHandlerCategory.Package };
+
+        public IActionHandlerResult Execute(IActionHandlerContext context)
+            => context.CalamariCommand(CalamariFlavour.CalamariAws, "upload-aws-s3")
+                .WithArgument("bucket", context.Variables.Get(AwsSpecialVariables.Action.Aws.S3.BucketName))
+                .WithArgument("targetMode", context.Variables.Get(AwsSpecialVariables.Action.Aws.S3.TargetMode))
+                .WithStagedPackageArgument()
+                .WithArgument("extensions",CalamariExtensions.Aws)
+                .Execute();
+    }
+}
