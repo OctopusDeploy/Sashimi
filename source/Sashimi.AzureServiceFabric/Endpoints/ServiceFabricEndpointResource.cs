7414446c43b53c2f287da3418a1e20830e8611e7
Shannon Lewis <slewis74@gmail.com>
2020-06-16 08:26:55 +1000

    Align ServiceFabric resource type with the model, re nullability

--- source/Sashimi.AzureServiceFabric/Endpoints/ServiceFabricEndpointResource.cs
+++ source/Sashimi.AzureServiceFabric/Endpoints/ServiceFabricEndpointResource.cs
@@ -1,5 +1,4 @@
-﻿#nullable disable
-using Octopus.Data.Resources;
+﻿using Octopus.Data.Resources;
 using Octopus.Data.Resources.Attributes;
 using Sashimi.Server.Contracts.Endpoints;
 
@@ -9,47 +8,45 @@ namespace Sashimi.AzureServiceFabric.Endpoints
     {
         public override CommunicationStyle CommunicationStyle => CommunicationStyle.AzureServiceFabricCluster;
 
-        [Trim]
-        [Writeable]
-        public string ConnectionEndpoint { get; set; }
+        [Trim] [Writeable] public string ConnectionEndpoint { get; set; } = string.Empty;
 
         [Trim]
         [Writeable]
-        public AzureServiceFabricSecurityMode SecurityMode { get; set; }
+        public AzureServiceFabricSecurityMode SecurityMode { get; set; } = default(AzureServiceFabricSecurityMode);
 
         [Trim]
         [Writeable]
-        public string ServerCertThumbprint { get; set; }
+        public string? ServerCertThumbprint { get; set; }
 
         [Trim]
         [Writeable]
-        public string ClientCertVariable { get; set; }
+        public string? ClientCertVariable { get; set; }
 
         [Trim]
         [Writeable]
-        public string CertificateStoreLocation { get; set; }
+        public string? CertificateStoreLocation { get; set; }
 
         [Trim]
         [Writeable]
-        public string CertificateStoreName { get; set; }
+        public string? CertificateStoreName { get; set; }
 
         [Trim]
         [Writeable]
-        public AzureServiceFabricCredentialType AadCredentialType { get; set; }
+        public AzureServiceFabricCredentialType? AadCredentialType { get; set; }
 
         [Trim]
         [Writeable]
-        public string AadClientCredentialSecret { get; set; }
+        public string? AadClientCredentialSecret { get; set; }
 
         [Trim]
         [Writeable]
-        public string AadUserCredentialUsername { get; set; }
+        public string? AadUserCredentialUsername { get; set; }
 
         [Writeable]
-        public SensitiveValue AadUserCredentialPassword { get; set; }
+        public SensitiveValue? AadUserCredentialPassword { get; set; }
 
         [Trim]
         [Writeable]
-        public string DefaultWorkerPoolId { get; set; }
+        public string DefaultWorkerPoolId { get; set; } = string.Empty;
     }
 }
