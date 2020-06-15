80f7d9a18dbaf89287b301d26818bdaa1b0d1c13
Frank Lin <frank.lin@octopus.com>
2020-06-15 15:13:31 +1000

    Remove AWS runscript handler registration, we haven't implemented the command yet

--- source/Sashimi.Aws/AwsModule.cs
+++ source/Sashimi.Aws/AwsModule.cs
@@ -20,10 +20,12 @@ namespace Sashimi.Aws
             builder.RegisterType<CloudFormationYamlCloudTemplateHandler>().As<ICloudTemplateHandler>().SingleInstance();
 
             builder.RegisterType<AwsUploadS3ActionHandler>().As<IActionHandler>().AsSelf().InstancePerLifetimeScope();
-            builder.RegisterType<AwsRunScriptActionHandler>().As<IActionHandler>().AsSelf().InstancePerLifetimeScope();
             builder.RegisterType<AwsRunCloudFormationActionHandler>().As<IActionHandler>().AsSelf().InstancePerLifetimeScope();
             builder.RegisterType<AwsDeleteCloudFormationActionHandler>().As<IActionHandler>().AsSelf().InstancePerLifetimeScope();
             builder.RegisterType<AwsApplyCloudFormationChangeSetActionHandler>().As<IActionHandler>().AsSelf().InstancePerLifetimeScope();
+
+            //Register when we've actually implemented the runscript command
+            //builder.RegisterType<AwsRunScriptActionHandler>().As<IActionHandler>().AsSelf().InstancePerLifetimeScope();
         }
     }
 }
