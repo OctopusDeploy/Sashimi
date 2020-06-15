e090ff1ceda8e3da08feba6f111f0c39a52da10a
Shannon Lewis <slewis74@gmail.com>
2020-06-15 17:12:26 +1000

    Updates to use wrapped request/responses

--- source/Sashimi.Azure/Web/AzureApi.cs
+++ source/Sashimi.Azure/Web/AzureApi.cs
@@ -4,7 +4,7 @@ using Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Api;
 
 namespace Sashimi.Azure.Web
 {
-    public class AzureApi : RegisterEndpoint
+    public class AzureApi : RegistersEndpoints
     {
         public const string AzureEnvironmentsPath = "/api/accounts/azureenvironments";
 
