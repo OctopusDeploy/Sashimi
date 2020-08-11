96dd0284f6b512d252404846937034c96d36fe85
John Simons <john.simons@octopus.com>
2020-08-11 16:08:57 +1000

    Fixed name of Calamari flavour

--- source/Sashimi.AzureCloudService/AzureConstants.cs
+++ source/Sashimi.AzureCloudService/AzureConstants.cs
@@ -7,6 +7,6 @@ namespace Sashimi.AzureCloudService
     static class AzureConstants
     {
         public static readonly ActionHandlerCategory AzureActionHandlerCategory = new ActionHandlerCategory("Azure", "Azure", 600);
-        public static CalamariFlavour CalamariAzure = new CalamariFlavour("Calamari.AzureCouldServices");
+        public static CalamariFlavour CalamariAzure = new CalamariFlavour("Calamari.AzureCloudServices");
     }
 }
