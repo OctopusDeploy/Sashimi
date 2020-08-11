09da6faee04571c3b400d5884b8f659e649edef5
Frank Lin <frank.lin@octopus.com>
2020-08-11 15:48:33 +1000

    Restore constants

--- source/Sashimi.Aws.Accounts/KnownVariables.cs
+++ source/Sashimi.Aws.Accounts/KnownVariables.cs
@@ -10,6 +10,16 @@ namespace Sashimi.Aws.Accounts
             {
                 public static readonly WellKnownVariable AccessKey = new WellKnownVariable("Octopus.Action.Amazon.AccessKey");
                 public static readonly WellKnownVariable SecretKey = new WellKnownVariable("Octopus.Action.Amazon.SecretKey");
+
+                public static readonly string AccountId = "Octopus.Action.AwsAccount.Variable";
+                public static readonly string UseInstanceRole = "Octopus.Action.AwsAccount.UseInstanceRole";
+                public static readonly string AwsRegion = "Octopus.Action.Aws.Region";
+                public static readonly string AssumeRole = "Octopus.Action.Aws.AssumeRole";
+                public static readonly string AssumedRoleArn = "Octopus.Action.Aws.AssumedRoleArn";
+                public static readonly string AssumedRoleSession = "Octopus.Action.Aws.AssumedRoleSession";
+                public static readonly string AssumeRoleSessionDurationSeconds = "Octopus.Action.Aws.AssumeRoleSessionDurationSeconds";
+                public static readonly string IamCapabilities = "Octopus.Action.Aws.IamCapabilities";
+                public static readonly string AssumeRoleExternalId = "Octopus.Action.Aws.AssumeRoleExternalId";
             }
         }
     }
