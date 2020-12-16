2fa9dddd5cce7c4025707ceb1b9a3f8f5019e129
slewis74 <slewis74@gmail.com>
2020-12-16 16:53:19 +1000

    error in one of the refactors for the CommandTestBuilder

--- source/Calamari.Tests.Shared/CommandTestBuilder.cs
+++ source/Calamari.Tests.Shared/CommandTestBuilder.cs
@@ -178,16 +178,14 @@ namespace Calamari.Tests.Shared
                 if (!context.withStagedPackageArgument)
                 {
                     var packageId = context.Variables.GetRaw("Octopus.Test.PackagePath");
-                    if (packageId == null)
-                        throw new InvalidOperationException("Octopus.Test.PackagePath variable value could not be found.");
                     if (File.Exists(packageId))
                     {
-                        var fileName = new FileInfo(packageId).Name;
-                        File.Copy(packageId, Path.Combine(workingPath, fileName));
+                        var fileName = new FileInfo(packageId!).Name;
+                        File.Copy(packageId!, Path.Combine(workingPath, fileName));
                     }
                     else if (Directory.Exists(packageId))
                     {
-                        Copy(packageId, workingPath);
+                        Copy(packageId!, workingPath);
                     }
                 }
             }
