40ff9c621b68d3ffe99b1b7db71a09ed525cd5af
Andrew Best <andrew.best@octopus.com>
2021-02-26 09:11:02 +1030

    Tests the account verifier

--- source/Sashimi.Azure.Accounts.Tests/AzureServicePrincipalAccountVerifierFixture.cs
+++ source/Sashimi.Azure.Accounts.Tests/AzureServicePrincipalAccountVerifierFixture.cs
@@ -4,8 +4,11 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Calamari.Tests.Shared;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using NSubstitute;
 using NUnit.Framework;
 using Octopus.Data.Model;
+using Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Api;
 
 namespace Sashimi.Azure.Accounts.Tests
 {
@@ -61,6 +64,28 @@ namespace Sashimi.Azure.Accounts.Tests
             accountDetails.Password = "invalid password".ToSensitiveString();
             accountDetails.InvalidateTokenCache(httpMessageHandler);
             Assert.That(() => accountDetails.CreateResourceManagementClient(httpMessageHandler), Throws.TypeOf<Microsoft.IdentityModel.Clients.ActiveDirectory.AdalServiceException>());
+        }
+
+        [Test]
+        public void Verify_ShouldNotCacheClientCredentials()
+        {
+            var httpMessageHandler = new TestHttpClientHandler();
+            var accountDetails = new AzureServicePrincipalAccountDetails
+            {
+                SubscriptionNumber = ExternalVariables.Get(ExternalVariable.AzureSubscriptionId),
+                ClientId = ExternalVariables.Get(ExternalVariable.AzureSubscriptionClientId),
+                TenantId = ExternalVariables.Get(ExternalVariable.AzureSubscriptionTenantId),
+                Password = ExternalVariables.Get(ExternalVariable.AzureSubscriptionPassword).ToSensitiveString()
+            };
+
+            var clientFactory = Substitute.For<IOctopusHttpClientFactory>();
+            clientFactory.HttpClientHandler.Returns(httpMessageHandler);
+
+            var verifier = new AzureServicePrincipalAccountVerifier(new Lazy<IOctopusHttpClientFactory>(clientFactory));
+            verifier.Verify(accountDetails);
+
+            accountDetails.Password = "InvalidPassword".ToSensitiveString();
+            verifier.Verify(accountDetails);
         }
     }
 
