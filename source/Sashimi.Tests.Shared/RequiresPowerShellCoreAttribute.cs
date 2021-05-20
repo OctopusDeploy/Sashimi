66be8e0d0736f0071238870e0b44a031d17cd314
Geoff Lamrock <geoff.lamrock@gmail.com>
2021-05-20 11:49:30 +1000

    Adds test attributes for windows powershell and powershell core

---
+++ source/Sashimi.Tests.Shared/RequiresPowerShellCoreAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Calamari.Common.Plumbing.Extensions;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+
+namespace Sashimi.Tests.Shared
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class RequiresPowerShellCoreAttribute : NUnitAttribute, IApplyToTest
+    {
+        public void ApplyToTest(Test test)
+        {
+            if (ScriptingEnvironment.SafelyGetPowerShellVersion().Major < 6)
+            {
+                test.RunState = RunState.Skipped;
+                test.Properties.Set(PropertyNames.SkipReason, "This test requires PowerShell Core (6) or newer.");
+            }
+        }
+    }
+}
