45a44e273bf3ce04298e430443e9ee1f581d7686
Matt Hilton <730704+mjhilton@users.noreply.github.com>
2022-05-09 12:46:10 +1000

    Update AzureCommonModule.cs

--- source/Azure.Common/AzureCommonModule.cs
+++ source/Azure.Common/AzureCommonModule.cs
@@ -5,6 +5,7 @@ using Sashimi.Azure.Common.Variables;
 using Sashimi.Server.Contracts.Actions.Templates;
 using Sashimi.Server.Contracts.Variables;
 
+
 namespace Sashimi.Azure.Common
 {
     public class AzureCommonModule : Module
